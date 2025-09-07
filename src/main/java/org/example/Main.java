48ae92387e348d49c9e0a4af497c626a23cd216a
Anna Tymchenko <git.user.ATymchenko@inno.tech>
2025-09-07 16:33:46 +0300

    Курсовой проект. Задание по теме ""Синтаксис языка" и “Базовые типы данных”"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Курсовой проект. Задание по теме - Как компилировать и запускать Java");
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println(("Длина текста: " + text.length()));
+        System.out.println("Курсовой проект. Задание по теме \"\"Синтаксис языка\" и “Базовые типы данных”\"");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число и нажмите <Enter>:");
+        int firstNumber = Integer.parseInt(scanner.nextLine()); // необходимо преобразовать string в int
+        System.out.println("Введите второе число и нажмите <Enter>:");
+        int secondNumber = Integer.parseInt(scanner.nextLine()); // необходимо преобразовать string в int
+
+        System.out.println("Сумма двух чисел = " + (firstNumber + secondNumber));
+        System.out.println("Разность двух чисел = " + (firstNumber - secondNumber));
+        System.out.println("Произведение двух чисел = " + (firstNumber * secondNumber));
+        System.out.println("Частное = " + ((double) firstNumber / secondNumber));
+
     }
 }
