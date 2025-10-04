30ec71e5acd3104aa169ffde430c1595592a3be2
Anna Tymchenko <git.user.ATymchenko@inno.tech>
2025-10-04 17:52:27 +0300

    Курсовой проект. Задание по теме Циклы

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,27 @@
 package org.example;
-
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Курсовой проект. Задание по теме \"\"Синтаксис языка\" и “Базовые типы данных”\"");
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число и нажмите <Enter>:");
-        int firstNumber = Integer.parseInt(scanner.nextLine()); // необходимо преобразовать string в int
-        System.out.println("Введите второе число и нажмите <Enter>:");
-        int secondNumber = Integer.parseInt(scanner.nextLine()); // необходимо преобразовать string в int
+        int validFileCount = 0;
 
-        System.out.println("Сумма двух чисел = " + (firstNumber + secondNumber));
-        System.out.println("Разность двух чисел = " + (firstNumber - secondNumber));
-        System.out.println("Произведение двух чисел = " + (firstNumber * secondNumber));
-        System.out.println("Частное = " + ((double) firstNumber / secondNumber));
-        scanner.close(); // Закрываем сканер
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String patch = scanner.nextLine();
+            File file = new File(patch);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Не верно указан путь к файлу");
+                continue;
+            }
+
+            validFileCount++;
+            System.out.println("Путь указан верно " + validFileCount);
+        }
     }
 }
